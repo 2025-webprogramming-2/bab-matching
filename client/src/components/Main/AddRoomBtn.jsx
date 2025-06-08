ac75029b37e5899ee130ebd7b826f7df19538d2f
xionng <leeso0717@naver.com>
2025-06-08 19:19:08 +0900

    style: 메인 로고 추가

--- client/src/components/Main/AddRoomBtn.jsx
+++ client/src/components/Main/AddRoomBtn.jsx
@@ -12,9 +12,13 @@ function AddRoomBtn() {
   };
   return (
     <>
-      <div className={styles.wrapper} onClick={handleAddRoom}>
-        <div>
-          <img className={styles.plusIcon} src="/assets/plus.png" alt="플러스아이콘" />
+      <div className={styles.wrapper}>
+        <img className={styles.logoIcon} src="/assets/logo.png" alt="로고" />
+
+        <div className={styles.container} onClick={handleAddRoom}>
+          <div>
+            <img className={styles.plusIcon} src="/assets/plus.png" alt="플러스아이콘" />
+          </div>
         </div>
       </div>
     </>
